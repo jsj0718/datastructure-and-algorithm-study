1d5292254ffdd893c1f678a25fcbd7896f7c7dd1
jsj0718 <jeongsejin718@gmail.com>
2022-02-10 23:36:28 +0900

    test 2

--- src/main/java/algorithm/lecture/ohheum/recursion/Code03.java
+++ src/main/java/algorithm/lecture/ohheum/recursion/Code03.java
@@ -1,7 +1,18 @@
 package algorithm.lecture.ohheum.recursion;
 
 public class Code03 {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+	 public static void main(String[] args) {
+        int[] data = {1, 2, 3, 4, 5};
+        int target = 5;
+        System.out.println(search(data, data.length, target));
+    }
+
+    public static int search(int[] data, int n, int target) {
+        for (int i=0; i<n; i++) {
+            if (data[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
