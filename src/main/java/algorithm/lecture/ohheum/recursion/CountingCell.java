b1e5c45c64608ba914943706f49f5365a30f5c92
jsj0718 <jeongsejin718@gmail.com>
2022-02-12 22:40:09 +0900

    재귀 함수 (백트래킹, 멱함수)

---
+++ src/main/java/algorithm/lecture/ohheum/recursion/CountingCell.java
@@ -0,0 +1,58 @@
+package algorithm.lecture.ohheum.recursion;
+
+public class CountingCell {
+    private static int N = 8;
+    private static int[][] blob = {
+            {1, 0, 0, 0, 0, 0, 0, 1},
+            {0, 1, 1, 0, 0, 1, 0, 0},
+            {1, 1, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0, 1, 0, 0},
+            {1, 0, 0, 0, 1, 0, 0, 1},
+            {0, 1, 1, 0, 0, 1, 1, 1},
+    };
+
+    private static final int BACKGROUND_PIXEL = 0;
+    private static final int IMAGE_PIXEL = 1;
+    private static final int VISITED_PIXEL = 2;
+    private static final int[][] DIRECTIONS = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1}, {0, 1},
+            {1, -1}, {1, 0}, {1, 1}
+    };
+
+    private static int total = 0;
+
+    public static void main(String[] args) {
+        printBlob();
+        System.out.println(countCell(1, 5));
+        printBlob();
+    }
+
+    private static void printBlob() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(blob[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static int countCell(int x, int y) {
+        if (x < 0 || y < 0 || x >= N || y >= N) {
+            return 0;
+        } else if (blob[x][y] != IMAGE_PIXEL) {
+            return 0;
+        } else {
+            blob[x][y] = VISITED_PIXEL;
+            total++;
+            for (int[] d : DIRECTIONS) {
+                countCell(x + d[0], y + d[1]);
+            }
+            return total;
+        }
+    }
+
+}
---
+++ src/main/java/algorithm/lecture/ohheum/recursion/PowerSet.java
@@ -0,0 +1,123 @@
+package algorithm.lecture.ohheum.recursion;
+
+import java.util.Scanner;
+
+public class PowerSet {
+    //문제 1
+    private static char data[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    private static int n = data.length;
+    private static boolean[] included = new boolean[n];
+
+    //문제 2
+    private static StringBuilder sb = new StringBuilder();
+    private static int N;
+    private static int M;
+    private static boolean[] visited;
+    private static int[] arr;
+
+    public static void main(String[] args) {
+        //문제1
+        powerSetV1(0);
+
+        //문제2
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        visited = new boolean[N + 1];
+        arr = new int[M];
+//        powerSetV2(0);
+//        powerSetV3(0);
+//        powerSetV4(0);
+        powerSetV5(0);
+        System.out.println(sb);
+    }
+
+    public static void powerSetV1(int k) {
+        if (k == n) {
+            for (int i = 0; i < n; i++) {
+                if (included[i]) {
+                    System.out.print(data[i]);
+                }
+            }
+            System.out.println();
+            return;
+        }
+        included[k] = false;
+        powerSetV1(k + 1);
+        included[k] = true;
+        powerSetV1(k + 1);
+
+    }
+
+    public static void powerSetV2(int level) {
+        if (level == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[level] = i;
+                powerSetV2(level + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void powerSetV3(int level) {
+        if (level == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[level] = i;
+                powerSetV3(level + 1);
+            }
+        }
+    }
+
+    public static void powerSetV4(int level) {
+        if (level == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[level] = i;
+            powerSetV4(level + 1);
+        }
+    }
+
+    public static void powerSetV5(int level) {
+        if (level == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (level != 0 && arr[level-1] > i) {
+                continue;
+            }
+
+            arr[level] = i;
+            powerSetV5(level + 1);
+        }
+    }
+
+}
