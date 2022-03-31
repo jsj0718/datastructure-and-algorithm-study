f92827afafe7a41ca8bd22a14e032692218081f4
jsj0718 <jeongsejin718@gmail.com>
2022-03-31 15:30:43 +0000

    kimtaewon algorithm start

---
+++ src/main/java/algorithm/lecture/kimtaewon/section1/Solution1_3.java
@@ -0,0 +1,55 @@
+package algorithm.lecture.kimtaewon.section1;
+
+import java.util.*;
+
+public class Solution1_3 {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] strList = s.split(" ");
+        for (String str : strList) {
+            if (isLongerThanFront(answer, str)) {
+                answer = str;
+            }
+        }
+
+        return answer;
+    }
+
+    public boolean isLongerThanFront(String frontStr, String backStr) {
+        if (frontStr.length() >= backStr.length()) {
+            return false;
+        }
+        return true;
+    }
+
+    public String solutionV2(String s) {
+        String answer = "";
+
+        int maxVal = Integer.MIN_VALUE;
+        int len, pos;
+        while ((pos = s.indexOf(" ")) != -1) {
+            String tmp = s.substring(0, pos);
+            len = tmp.length();
+            if (len > maxVal) {
+                maxVal = len;
+                answer = tmp;
+            }
+
+            s = s.substring(pos + 1);
+        }
+
+        if (s.length() > maxVal) answer = s;
+
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+        
+        Solution1_3 T = new Solution1_3();
+        System.out.println(T.solutionV2(s));
+    }
+}
