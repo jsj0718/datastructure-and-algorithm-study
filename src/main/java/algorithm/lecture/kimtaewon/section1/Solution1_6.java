a93253c102021026ead85d18e1c49dbed973e83c
jsj0718 <jeongsejin718@gmail.com>
2022-04-01 09:06:25 +0000

    solve to String problem of kimtaewon

---
+++ src/main/java/algorithm/lecture/kimtaewon/section1/Solution1_6.java
@@ -0,0 +1,41 @@
+package algorithm.lecture.kimtaewon.section1;
+
+import java.util.*;
+
+public class Solution1_6 {
+    
+    public String solution(String str) {
+        String answer = "";
+
+        char[] cArr = str.toCharArray();
+        for (char c : cArr) {
+            if (!answer.contains(String.valueOf(c))) {
+                answer += c;
+            }
+        }
+
+        return answer;
+    }
+
+    public String solutionV2(String str) {
+        String answer = "";
+
+        for (int i=0; i<str.length(); i++) {
+            if (i == str.indexOf(str.charAt(i))) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+        
+        Solution1_6 T = new Solution1_6();
+//        System.out.println(T.solution(str));
+        System.out.println(T.solutionV2(str));
+    }
+}
