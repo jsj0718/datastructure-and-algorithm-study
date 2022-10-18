92ae6b0b73258b3d1f3ecf64a40741132b8d1f30
jsj0718 <jeongsejin718@gmail.com>
2022-10-19 00:14:48 +0900

    level1. 최소직사각형 문제 풀이

--- algorithm/src/test/java/programmers/level1/Level1Test.java
+++ algorithm/src/test/java/programmers/level1/Level1Test.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.Stack;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -74,4 +76,22 @@ public class Level1Test {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource(value = "최소직사각형_데이터")
+    void 최소직사각형(int[][] sizes, int result) {
+        //when
+        int height = IntStream.range(0, sizes.length).map(i -> Math.min(sizes[i][0], sizes[i][1])).max().getAsInt();
+        int width = Arrays.stream(sizes).flatMapToInt(arrays -> Arrays.stream(arrays)).max().getAsInt();
+
+        //then
+        assertThat(height * width).isEqualTo(result);
+    }
+    
+    static Stream<Arguments> 최소직사각형_데이터() {
+        return Stream.of(
+                Arguments.of(new int[][] {{60, 50}, {30, 70}, {60, 30}, {80, 40}}, 4000),
+                Arguments.of(new int[][] {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}}, 120),
+                Arguments.of(new int[][] {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}}, 133)
+        );
+    }
 }
